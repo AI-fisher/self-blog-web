d48dab22ab1bd66b44ea38bc9f3d17da278a4717
suchen <suchen@zj.aibao.com>
2021-08-11 11:15:12 +0800

    Vue-cli 重新构建

---
+++ babel.config.js
@@ -0,0 +1,6 @@
+/**
+ * babel 配置
+ */
+module.exports = {
+  presets: ["@vue/app"]
+};
---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+/**
+ * 后处理 css
+ */
+module.exports = {
+  plugins: {
+    // 自动加入不同浏览器的兼容前缀
+    autoprefixer: {}
+  }
+};
